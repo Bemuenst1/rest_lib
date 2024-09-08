550a48c2afe61a5153ae3774e07edbb5635d1c6f
Beni <inf22048@lehre.dhbw-stuttgart.de>
2024-09-08 20:26:49 +0200

    [edit] using decorator as function-wrapper to configure routes (idea of GetarnterStein)

--- example.py
+++ example.py
@@ -1,32 +1,24 @@
-import json
-
-from rest import API
-from threading import Thread
+from rest import API, route
 
 class Handler:
-    @staticmethod
-    def hello(name):
-        return {"message": f"Hello, {name}!"}
     
-    @staticmethod
-    def goodbye(name):
+    @route("/hello", ["GET"], ["name"])
+    def hello(self, name):
+        return {"message": f"Hello, {name}!"}
+
+    @route("/goodbye", ["GET"], ["name"])
+    def goodbye(self, name):
         return {"message": f"Goodbye, {name}!"}
 
-def load_config():
-    with open("api_config.json", "r") as f:
-        return json.loads(f.read())
-
-def dummy_worker():
-    while True:
-        print(input("Enter something: "))
+    @route("/test")
+    def test(self):
+        return "test"
 
 def main():
-    #dummy_thread = Thread(target=dummy_worker)
-    #dummy_thread.start()
 
-    API(api_config=load_config(), handler=Handler()).start()
+    api_keys = ["12345678", "abcdefgh"]	
 
-    #dummy_thread.join()
+    API(api_keys, api_handler=Handler()).start()
 
 if __name__ == "__main__":
     main()
--- rest.py
+++ rest.py
@@ -1,55 +1,52 @@
 from flask import Flask, request, jsonify
 
+def route(path, methods=['GET'], params=[]):
+    def decorator(func):
+        func.path = path
+        func.methods = methods
+        func.params = params
+        return func
+    return decorator
+
 class API(object):
     app = Flask(__name__)
 
-    def __new__(cls, api_config=None, handler=None):
+    def __new__(cls, api_keys=None, api_handler=None):
         if not hasattr(cls, 'instance'):
             cls.instance = super(API, cls).__new__(cls)
-            cls.instance.init(api_config, handler)
-
+            cls.instance.init(api_keys, api_handler)
         return cls.instance
     
     @staticmethod
     def start():
         API.app.run(debug=True, threaded=True, host='0.0.0.0')
+    
+    @staticmethod
+    def stop(self):
+        API.app.shutdown()
 
-    def init(self, api_config, handler):
-        self.config = api_config
-        self.handler = handler
-
-        self.api_keys = set(self.config.get('api_keys', []))
-
-        # 404 route
-        @self.app.errorhandler(404)
-        def not_found(error):
-            return jsonify({"error": "Not found"}), 404
+    def init(self, api_keys, api_handler):
+        self.keys = api_keys
+        self.handler = api_handler
 
         # initialize routes
-        for route in self.config.get('routes', []):
-            self.add_route(route)
-    
-    def add_route(self, route):
-        # route configuration
-        path = route.get('path', None)
-        func_name = route.get('func', None)
-        methods = route.get('methods', ['GET'])
-        params = route.get('params', [])
+        for func_name in dir(self.handler):
+            func = getattr(self.handler, func_name)
 
-        if not path or not func_name:
+            if callable(func) and hasattr(func, 'path'):
+                self.add_route(func.path, func, func.methods, func.params)
+
+    
+    def add_route(self, path, func, methods, params):
+        if not path:
             print("[Error]: Invalid route configuration")
             return
 
-        @self.app.route(path, methods=methods, endpoint=func_name)
-        def route_handler(func_name=func_name, params=params):
+        @self.app.route(path, methods=methods, endpoint=func.__name__)
+        def route_handler(func=func, params=params):
             # API key validation
             if not self.validate_key():
                 return self.error("Invalid API key")
-            
-            # load handler function
-            func = getattr(self.handler, func_name, None)
-            if not func:
-                return self.error("Invalid handler function")
             
             # collect arguments from parameters
             args = {param: request.args.get(param) for param in params}
@@ -61,10 +58,14 @@ class API(object):
 
     def validate_key(self):
         request_key = request.headers.get('key', None)
-        if not request_key or not request_key in self.api_keys:
+        if not request_key or not request_key in self.keys:
             return False
         return True
         
     def error(self, message):
         print(f"[Error]: {message}")
         return jsonify({"error": message}), 400
+    
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({"error": "Not found"}), 404
